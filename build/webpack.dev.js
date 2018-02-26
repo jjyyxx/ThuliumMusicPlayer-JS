977145d3052a10a70628876a554b3a646f0d94b4
jjyyxx <obfishstudio@gmail.com>
2018-02-26 21:37:40 +0800

    migrate to webpack v4

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
+    mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
